fa30ab9fb443ca40af976d3776bdca282a5e8e2a
sg23x <soumyagupta2307@gmail.com>
2020-07-14 01:17:49 +0530

    added: orientation lock to portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:psych/UI/screens/nameInput.dart';
 
-main() => runApp(
+main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then(
+    (value) => runApp(
       MyApp(),
-    );
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
